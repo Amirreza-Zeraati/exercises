145fc8c40954794d132560d89cd46e9b689df2f1
Amirreza Zeraati <iamzeraati@gmail.com>
2023-09-19 21:41:04 +0330

    Update PasswordGenerator.py

--- PasswordGenerator/PasswordGenerator.py
+++ PasswordGenerator/PasswordGenerator.py
@@ -1,11 +1,28 @@
 from random import shuffle, choice
-from string import ascii_letters, digits
+from string import ascii_letters, digits, punctuation
 
 
+'''
+    we can use punctuation; but we dont need that much special characters
+    punctuation :  !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~
+    if you like it, do this:
+    characters = list(ascii_letters + digits + punctuation)
+'''
 characters = list(ascii_letters + digits + "[]{}()*&^%$#@!~")
 
 
 def PasswordGenerator():
+    '''
+        at first we have this
+        ['a', 'b', 'c', 'd', ... , '&', '*', '(', ')']
+        then we shuffle it to pick randomly
+        ['v', 'o', '0', '*', ... , 'O', '$', 'V', 'u', '8']
+        after pick enough, shuffle it again or more shure
+        length = 8
+        ['e', 'r', '6', 'n', 'j', '*', 'q', 'S']
+        by using .join() method we finally got password
+        --  er6nj*qS  --
+    '''
     length = int(input("Enter Password Length : "))
     shuffle(characters)
     password = [choice(characters) for i in range(length)]
