f2206b188e2210d9f0c4cb970d1c3a7ff240876e
Amirreza Zeraati <iamzeraati@gmail.com>
2023-09-29 21:18:55 +0330

    Fix BinarySearchAlgorithm.py

--- BinarySearchAlgorithm.py
+++ BinarySearchAlgorithm.py
@@ -15,6 +15,8 @@ def binary_search(array, x):
 
 
 def binary_search_recursive(array, first, last, x):
+    # in recursive, the array have to be sorted first
+    array.sort()
     if len(array) == 0:
         return False
     else:
@@ -35,5 +37,5 @@ def binary_search_recursive(array, first, last, x):
 
 if __name__ == '__main__':
     array = [102, 4, 7, 1, 19, 14, 28, 211, 3, 32]
-    print('Binary Search : ', binary_search(array, 5))
-    print('Binary Search Recursive : ', binary_search_recursive(array, 0, len(array) - 1, 5))
+    print('Binary Search : ', binary_search(array, 61)) # False
+    print('Binary Search Recursive : ', binary_search_recursive(array, 0, len(array) - 1, 4)) # True
