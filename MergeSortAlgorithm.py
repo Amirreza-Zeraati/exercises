b3b9c29c5df75e68a9ed0bf6f22a440ce96f229f
Amirreza Zeraati <iamzeraati@gmail.com>
2023-10-01 10:15:54 +0330

    Update MergeSortAlgorithm.py

--- MergeSortAlgorithm.py
+++ MergeSortAlgorithm.py
@@ -1,6 +1,6 @@
 def merge_sort(arr):
     print('Dividing :', arr)
-
+    
     if len(arr) > 1:
         mid_point = len(arr) // 2
         left_half = arr[:mid_point]
@@ -21,13 +21,18 @@ def merge_sort(arr):
                 arr[k] = right_half[j]
                 j += 1
             k += 1
-
         while i < len(left_half):
             arr[k] = left_half[i]
             i += 1
             k += 1
-
         while j < len(right_half):
             arr[k] = right_half[j]
             j += 1
             k += 1
+    print("Merging :", arr)
+
+
+if __name__ == '__main__':
+    arr = [45, 7, 85, 24, 60, 255, 38, 63, 1]
+    merge_sort(arr)
+    print('Sorted Array :', arr)
