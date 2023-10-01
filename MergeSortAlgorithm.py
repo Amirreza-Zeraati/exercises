ffe492f3991430355309a0d7bd2fc9b8ab4de742
Amirreza Zeraati <iamzeraati@gmail.com>
2023-10-01 10:06:23 +0330

    Add files via upload

---
+++ MergeSortAlgorithm.py
@@ -0,0 +1,33 @@
+def merge_sort(arr):
+    print('Dividing :', arr)
+
+    if len(arr) > 1:
+        mid_point = len(arr) // 2
+        left_half = arr[:mid_point]
+        right_half = arr[mid_point:]
+
+        merge_sort(left_half)
+        merge_sort(right_half)
+
+        i = 0
+        j = 0
+        k = 0
+
+        while i < len(left_half) and j < len(right_half):
+            if left_half[i] <= right_half[j]:
+                arr[k] = left_half[i]
+                i += 1
+            else:
+                arr[k] = right_half[j]
+                j += 1
+            k += 1
+
+        while i < len(left_half):
+            arr[k] = left_half[i]
+            i += 1
+            k += 1
+
+        while j < len(right_half):
+            arr[k] = right_half[j]
+            j += 1
+            k += 1
